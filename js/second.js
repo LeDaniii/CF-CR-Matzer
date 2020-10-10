734e983a40d135d87693996d13093287fa3396e4
LeDaniii <danielmatzer@gmail.com>
2020-10-11 01:25:28 +0200

    thumbs up increment on second html works but it increments also with the thumbs down button Fix is needed

--- js/second.js
+++ js/second.js
@@ -1,12 +1,89 @@
+var moviesJson = JSON.parse(movieList);
+console.table(moviesJson);
+
+function displaySlider() {
+    const movieSlider = document.getElementById("swiper-wrapper");
+    // hard reset if needed / don't know yet
+    movieSlider.innerHTML = "";
+    for (let i = 0; i < moviesJson.length; i++) {
+        movieSlider.innerHTML += `           
+         <div class="swiper-slide">
+              <div class="movie-card">
+                <div class="movie-img">
+                  <img src="${moviesJson[i].image}" alt="" />
+                </div>
+                <div class="movie-description">
+                  <div class="card-title">
+                    <h1>${moviesJson[i].name}</h1>
+                  </div>
+                  <div class="card-description">
+                    <p>
+                        ${moviesJson[i].description}
+                    </p>
+                    <div class="card-starring">
+                      <h2>starring</h2>
+                      <p>${moviesJson[i].starring}</p>
+                    </div>
+                    <div class="card-runtime">
+                      <p>runtime</p>
+                      <p>${moviesJson[i].runtime}</p>
+                      <button class="card-trailer-btn">
+                        <i class="fas fa-play"></i> Watch Trailer
+                      </button>
+                    </div>
+                    <div class="card-booking">
+                      <div class="booking-area">${moviesJson[i].bookingToday}</div>
+                      <div class="booking-area">${moviesJson[i].bookingToday}</div>
+                      <div class="booking-area">${moviesJson[i].bookingToday}</div>
+                      <div class="booking-area">${moviesJson[i].bookingToday}</div>
+                      <div class="booking-area">${moviesJson[i].bookingToday}</div>
+                      <div class="booking-area">${moviesJson[i].bookingToday}</div>
+                    </div>
+                  </div>
+                  <div class="card-footer">
+                    <div class="opinion-bar">
+                      <p id="down${i} class="counter">
+                        <i class="fas fa-thumbs-down"></i> ${moviesJson[i].likes}
+                      </p>
+                      <p id="up${i}" class="counter"><i class="fas fa-thumbs-up"></i> ${moviesJson[i].likes}</p>
+                    </div>
+                    <div class="opinion-buttons">
+                      <button id="${i}" class="opinion-button-red">
+                        <span><i class="fas fa-thumbs-down"></i></span> your
+                        opinion
+                      </button>
+                      <button id="${i}" class="opinion-button">
+                        <span><i class="fas fa-thumbs-up"></i></span> your
+                        opinion
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>`
+    }
+}   
+
+// ---------- Calling displaySlider ---------
+
+displaySlider()
+
+// ---------- Like Button Increment ---------
 
 
+// ################ Does not work with two buttons #################
+// ----- EventListener on the Card-Container ------
+let swiperLogic = document.querySelector("#swiper-wrapper");
+swiperLogic.addEventListener("click", counterLogic, false);
 
-
-
-
-
-
-
+function counterLogic(e) {
+    let j = e.target.id;
+    console.log(j);
+    moviesJson[j].likes += 1;
+    let likeCounter = document.getElementById(`up${j}`);
+    likeCounter.innerHTML = `<span id="counter ${j}" class="text-warning my-auto"><i class="fas fa-thumbs-up"></i> ${moviesJson[j].likes}</span>`
+}
+// ##################################################################
 
 
 
@@ -39,3 +116,6 @@ var mySwiper = new Swiper('.swiper-container', {
     el: '.swiper-scrollbar',
   },
 })
+
+
+
